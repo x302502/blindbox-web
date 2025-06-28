36ac3a5ee521695b6566451a19d316221fca576e
thanhluan.bkhn <thanhluan.bkhn@gmail.com>
2025-06-28 13:43:59 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,6 @@ import { MainLayout } from "./layouts/main-layout";
 import { ConfigProvider } from "antd";
 import { useThemeStore } from "./stores/theme/useThemeStore";
 import { DemoPage } from "./pages/demo";
-import {
-  CoinbaseWallet,
-  MetaMask,
-  OkxWallet,
-  WagmiWeb3ConfigProvider,
-} from "@ant-design/web3-wagmi";
-import { ChainType, Web3ConfigProvider } from "@ant-design/web3";
-import HomePage from "./pages/home";
-import LootBox from "./components/LootBox/LootBox";
 
 const App: React.FC = () => {
   const { themConfig } = useThemeStore();
@@ -21,59 +12,9 @@ const App: React.FC = () => {
         ...themConfig,
       }}
     >
-      <Web3ConfigProvider
-        theme={{
-          ...themConfig,
-        }}
-      >
-        <WagmiWeb3ConfigProvider
-          eip6963={{
-            autoAddInjectedWallets: true,
-          }}
-          ens
-          wallets={[MetaMask(), CoinbaseWallet(), OkxWallet()]}
-          chains={[
-            {
-              id: 10143,
-              name: "Monad testnet",
-              nativeCurrency: {
-                name: "MON",
-                symbol: "MON",
-                decimals: 18,
-              },
-              type: ChainType.EVM,
-              wagmiChain: {
-                id: 10143,
-                name: "Monad testnet",
-                nativeCurrency: {
-                  name: "MON",
-                  symbol: "MON",
-                  decimals: 18,
-                },
-                rpcUrls: {
-                  default: {
-                    http: [
-                      "https://monad-testnet.drpc.org",
-                      "https://testnet-rpc.monad.xyz",
-                    ],
-                    webSocket: ["wss://monad-testnet.drpc.org"],
-                  },
-                },
-                blockExplorers: {
-                  default: {
-                    name: "SocialScan",
-                    url: "https://monad-testnet.socialscan.io",
-                  },
-                },
-              },
-            },
-          ]}
-        >
-          <MainLayout>
-            <LootBox />
-          </MainLayout>
-        </WagmiWeb3ConfigProvider>
-      </Web3ConfigProvider>
+      <MainLayout>
+        <DemoPage />
+      </MainLayout>
     </ConfigProvider>
   );
 };
--- src/AppRoot.tsx
+++ src/AppRoot.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
+
 const composeProviders =
   (...providers: any[]) =>
   (Child: any) =>
